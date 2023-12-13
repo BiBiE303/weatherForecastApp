b7851bced450eb9bf3656fd412026ad768edc5e8
BiBiE303 <128813735+BiBiE303@users.noreply.github.com>
2023-12-13 23:36:16 +0100

    Made some changes and added a forecast to the javascript

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function displayTemp(response) {
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#input-city");
-  let city = searchInputElement.value;
+  searchCity(searchInputElement.value);
+}
+function searchCity(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=82ob938a110551c57b64tf4cacd5f1d1`;
   axios.get(apiUrl).then(displayTemp);
 }
@@ -58,3 +60,45 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "82ob938a110551c57b64tf4cacd5f1d1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showForecast);
+}
+function showForecast(response) {
+  console.log(response.data);
+  let forecastHtml = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+  <div class="forecast-day">
+            <div class="forecast-date">${formatDay(day.time)}</div>
+           <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+            <div class="forecast-temparatures">
+              <div class="forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
+              </div>
+              <div class="forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}º</div>
+            </div>
+            </div>
+            `;
+    }
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+searchCity("Lagos");
